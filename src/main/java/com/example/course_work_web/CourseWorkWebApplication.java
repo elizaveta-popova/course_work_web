adbceeda002564141094d5bc6974e1a132e579b2
eliza <eliza.an.popova@gmail.com>
2023-04-11 02:16:41 +0300

    first

---
+++ src/main/java/com/example/course_work_web/CourseWorkWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_work_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/course_work_web/CourseWorkWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_work_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
